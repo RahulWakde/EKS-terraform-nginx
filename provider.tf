b379e0598ebe810c8b05dd4889274a8448a0241c
Ubuntu <ubuntu@ip-172-31-39-114.ap-south-1.compute.internal>
2026-01-28 09:35:47 +0000

    Terraform version updated

--- provider.tf
+++ provider.tf
@@ -1,4 +1,19 @@
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.30"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.25"
+    }
+  }
+}
+
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
 }
 
